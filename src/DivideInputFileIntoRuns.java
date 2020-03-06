b92c6b4056778c53b2b8dec3683b8fb7ac4418d8
hammam98 <45409627+hammam98@users.noreply.github.com>
2020-03-06 05:59:23 +0200

    Add nameGenerator function

--- src/DivideInputFileIntoRuns.java
+++ src/DivideInputFileIntoRuns.java
@@ -1,17 +1,33 @@
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
 
 
 public class DivideInputFileIntoRuns {
 
+
+   ArrayList<String> nameGenerator(int fileLength, int runSize){
+        float numberOfRuns=((fileLength/8)/runSize);
+        int counter=0;
+       ArrayList<String> ar = new ArrayList<String>();
+       for(float i=0;i<numberOfRuns;i++) {
+            counter++;
+            String strI="Run"+counter+".bin";
+            ar.add(strI);
+        }
+       return ar;
+    }
+
+    public
     void DivideInputFileIntoRuns (String Inputfilename, int runSize)throws Exception
     {
-        String pathes[] = {"run1.bin", "run2.bin", "run3.bin", "run4.bin", "run5.bin", "run6.bin", "run7.bin", "run8.bin", "run9.bin", "run10.bin", "run11.bin","run12.bin", "run13.bin", "run14.bin", "run15.bin","runs16.bin","runs17.bin"};
-        RandomAccessFile fileStore1 = new RandomAccessFile(Inputfilename, "rw");
-        for(int j=0;j<(fileStore1.length()/8)/runSize;j++)//loop number of runs <how many run>(number of record / run size)
+        RandomAccessFile mainFile = new RandomAccessFile(Inputfilename, "rw");
+        ArrayList<String> pathes = nameGenerator(Inputfilename.length(),runSize);
+
+        for(int j=0;j<(mainFile.length()/8)/runSize;j++)//loop number of runs <how many run>(number of record / run size)
         {
-            RandomAccessFile fileStore2 = new RandomAccessFile(pathes[j], "rw");//open new physical file on same logic file
+            RandomAccessFile fileForRun = new RandomAccessFile(pathes.get(j), "rw");//open new physical file on same logic file
             for (int i = 0; i < runSize * 2; i++)//will loop N*2 times each run (i put *2 that record have tow integer)
-                fileStore2.writeInt(fileStore1.readInt());// will write record
+                fileForRun.writeInt(mainFile.readInt());// will write record
         }
         // now runs have been fulled , BUT may there remain record didn't wrote in run
         //ex: there 4 record the run size 3 will remain 1 , so i tried to read until end of file , i couldn't
@@ -24,16 +40,16 @@ public class DivideInputFileIntoRuns {
         //here i get idea to solve it , i use %
         //record number % run size =will get me the last run size
         RandomAccessFile fileStore2 = new RandomAccessFile("remainder.bin", "rw");
-        long remainRecord=(fileStore1.length()/8)%runSize;
+        long remainRecord=(mainFile.length()/8)%runSize;
         for (int i = 0; i < remainRecord * 2; i++)//will loop N*2 times each run (i put *2 that record have tow integer)
-            fileStore2.writeInt(fileStore1.readInt());// will write record
+            fileStore2.writeInt(mainFile.readInt());// will write record
 
 
 
                              /////   just for test it's just print /////
-        for(int j=0;j<(fileStore1.length()/8)/runSize;j++)//loop number of runs<how many runs> (number of record / run size)
+        for(int j=0;j<(mainFile.length()/8)/runSize;j++)//loop number of runs<how many runs> (number of record / run size)
         {
-            RandomAccessFile fileStore3 = new RandomAccessFile(pathes[j], "rw");
+            RandomAccessFile fileStore3 = new RandomAccessFile(pathes.get(j), "rw");
             for (int i = 0; i < runSize * 2; i++)// will loop N*2 times each run (i put *2 that record have tow integer)
             {
                 System.out.print(fileStore3.readInt()+"    ");// will write record
@@ -43,13 +59,13 @@ public class DivideInputFileIntoRuns {
         }
 
         RandomAccessFile fileStore3 = new RandomAccessFile("remainder.bin", "rw");
-        long remainRecord1=(fileStore1.length()/8)%runSize;
+        long remainRecord1=(mainFile.length()/8)%runSize;
         for (int i = 0; i < remainRecord1 * 2; i++)//will loop N*2 times each run (i put *2 that record have tow integer)
         {
             System.out.print(fileStore3.readInt()+"   ");// will write record
             if(i%2!=0){System.out.println(" ");}
         }
-        System.out.println("-----------Run number : "+ (((fileStore1.length()/8)/runSize)+1)+ "-------------");
+        System.out.println("-----------Run number : "+ (((mainFile.length()/8)/runSize)+1)+ "-------------");
                             //////  JUST FOR TEST //////
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.FileInputStream;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -7,6 +8,7 @@ public class Main {
     public static void main(String[] args) throws Exception  {
         System.out.println("himo");
         DivideInputFileIntoRuns ob =new DivideInputFileIntoRuns();
-        ob.DivideInputFileIntoRuns("Index.bin",5);
+        ob.DivideInputFileIntoRuns("Index.bin",4);
+
     }
 }
